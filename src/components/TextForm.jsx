fa361224d85bbf9f5bad7abc5c5ca493c7ac3572
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-01 21:53:31 +0500

    Video 8 Completed

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,25 +1,50 @@
 import { useState } from "react";
 
 function TextForm(props) {
-    const handleUpperCase = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
+  const handleUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleOnChange = (event) => {
-        setText(event.target.value);
-    }
+  const handleLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const [text, setText] = useState("Enter text here");
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
 
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpperCase}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowerCase}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpperCase}>Convert to Uppercase</button>
-    </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>Requires {0.008 * text.split(" ").length} minutes to read.</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
 
