7f1f6b4d634206a75244ee2417a092d5a2f76e71
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-02 15:31:31 +0500

    Video 11 Completed (Added more functions into TextForm.jsx)

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -16,6 +16,17 @@ function TextForm(props) {
     setText(newText);
   };
 
+  const handleCopyText = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -43,6 +54,12 @@ function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-1" onClick={handleClearText}>
           Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyText}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove extra spaces
         </button>
       </div>
       <div className="container my-3">
