91ed0b1f1b72c1a9e3e43728afb6d2be118a4c8c
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-02 16:12:02 +0500

    Video 12 Completed (Added Dark Mode Functionality)

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import { useState } from "react";
 
 function App() {
+
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light"){
+      setMode("dark"); 
+      document.body.style.backgroundColor = "grey";
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        <TextForm heading="Enter text to analyze below" />
+        <TextForm heading="Enter text to analyze below" mode={mode}/>
       </div>
       {/* <About /> */}
     </>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -20,12 +20,12 @@ function TextForm(props) {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
-  }
+  };
 
   const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
-  }
+  };
 
   const handleOnChange = (event) => {
     setText(event.target.value);
@@ -35,7 +35,7 @@ function TextForm(props) {
 
   return (
     <>
-      <div>
+      <div className="container" style={{color: props.mode === "dark" ? "white" : "black"}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
@@ -44,6 +44,10 @@ function TextForm(props) {
             rows="8"
             value={text}
             onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpperCase}>
@@ -62,12 +66,14 @@ function TextForm(props) {
           Remove extra spaces
         </button>
       </div>
-      <div className="container my-3">
+      <div className="container my-3" style={{color: props.mode === "dark" ? "white" : "black"}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>
+          {text.split(" ").length} words and {text.length} characters.
+        </p>
         <p>Requires {0.008 * text.split(" ").length} minutes to read.</p>
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here."}</p>
       </div>
     </>
   );
