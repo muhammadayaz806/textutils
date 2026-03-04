493d9a5d473401b5e214b0a3b29249aecf7bb97e
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-04 15:45:32 +0500

    Video 16 Completed (Added React-Router-Dom)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
-
   const [mode, setMode] = useState("light");
 
   const toggleMode = () => {
-    if (mode === "light"){
-      setMode("dark"); 
+    if (mode === "light") {
+      setMode("dark");
       document.body.style.backgroundColor = "grey";
-    }else{
+    } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
     }
-  }
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <div className="container my-3">
-        <TextForm heading="Enter text to analyze below" mode={mode}/>
-      </div>
-      {/* <About /> */}
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <div className="container my-3">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TextForm heading="Enter text to analyze below" mode={mode} />
+              }
+            />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
